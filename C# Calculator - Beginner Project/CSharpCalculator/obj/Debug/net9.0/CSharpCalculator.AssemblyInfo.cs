284eedadb25a8fc6740311b851e6978ac37408af
Ivan Ivanov <ivan78608@gmail.com>
2025-01-21 00:04:53 +0200

    Corrected the names for the two created console applications Console Calculator and Console Dice Game

--- C# Calculator - Beginner Project/CSharpCalculator/obj/Debug/net9.0/CSharpCalculator.AssemblyInfo.cs
+++ C# Calculator - Beginner Project/CSharpCalculator/obj/Debug/net9.0/CSharpCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+715bd10a921d7d0e592b9512978b0ad27c3279e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ad23f7cc52c22619c7de4747d7d83fc65f34e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
