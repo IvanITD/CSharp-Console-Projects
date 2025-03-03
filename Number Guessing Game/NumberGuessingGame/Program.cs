438180ba893f531c82657ee07b3e414f78bbed00
Ivan Ivanov <ivan78608@gmail.com>
2025-03-03 23:44:51 +0200

    Created the greeting for the game

--- Number Guessing Game/NumberGuessingGame/Program.cs
+++ Number Guessing Game/NumberGuessingGame/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿// Greet the user to the Number Guessing Game!
+Console.WriteLine("Hello user!");
+// Creating a delay in order to make it possible for the user to read the greeting.
+System.Threading.Thread.Sleep(1500);
+Console.WriteLine("Welcome to my NUMBER GUESSING GAME");
+
